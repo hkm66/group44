ca6c6c6713bf184261a6099e6ebdaf432329836c
ibrahimBLT <ahundekbulut@gmail.com>
2022-12-16 22:57:48 +0300

    test1 changed

--- src/test1.java
+++ src/test1.java
@@ -1,5 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class test1 {
     public static void main(String[] args) {
+
+        System.out.println("wooden spoon");
+
         System.out.println("hello world");
     }
+
 }
